84b7b8c80982c4e937999ad6eed1c04fc5447fce
andres1112 <afo_1112@hotmail.com>
2017-03-21 20:40:25 -0500

    Update Matricula.php

--- application/controllers/Matricula.php
+++ application/controllers/Matricula.php
@@ -61,4 +61,52 @@ class Matricula extends CI_Controller {
 
   }
 
+
+
+function buscar_codigof($opcion = null){
+    $this->load->model('Matricula_model');
+
+
+    if($opcion=="formulario"){
+
+      $this->load->view('buscar_codigo');
+
+    }else if($opcion=="validar"){
+
+      $this->form_validation->set_rules('id_estu', 'id_estu', 'required');
+
+       if ($this->form_validation->run() == FALSE){
+            
+            $this->load->view('buscar_codigo');
+
+        }else{
+
+            $value['id_estudiante'] = $this->input->post('id_estu');
+            $matricula = new Matricula_model($value);
+            $matricula->obtener_curso_por_estudiante();
+            $data['matricula'] = $matricula;
+            //echo "<p><pre>".var_dump($matricula)."</pre></p>";
+            $cursos = $matricula->obtener_cursos();
+            $data['cursos'] = $cursos;
+            //var_dump($cursos);
+            $estudiantes = $matricula->obtener_estudiantes();
+            $data['estudiantes'] = $estudiantes;
+            //var_dump($matricula);
+            //var_dump($estudiantes);
+            //var_dump($cursos);
+            //echo "<p><pre>".var_dump($cursos)."</pre></p>";
+            $this->load->view('listar_est_cod',$data);
+
+        }
+
+    }
+  }
+
+
+
+
+
+
+
+
 }
